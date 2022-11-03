879d2b7716d9bc3964ee0583b80e0866614ac935
pengdst <pengkuh.dst@gmail.com>
2022-11-04 02:27:54 +0700

    create loader config

---
+++ configs/env_conf.go
@@ -0,0 +1,26 @@
+package configs
+
+import (
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+type EnvConf struct {
+	RedisUrl string `env:"REDIS_URL,required"`
+}
+
+func NewEnv() EnvConf {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("cannot load env: %v", err)
+	}
+
+	var envConf EnvConf
+	errConf := env.Parse(&envConf)
+	if errConf != nil {
+		log.Fatalf("cannot load env: %v", errConf)
+	}
+
+	return envConf
+}
