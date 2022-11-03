d28217275854d91174f2d8d05c41d4c35d3fa48b
pengdst <pengkuh.dst@gmail.com>
2022-11-04 02:10:53 +0700

    delete unused task priority constants

--- tasks/type/constants.go
+++ tasks/type/constants.go
@@ -4,9 +4,3 @@ const (
 	TypeEmailDelivery = "email:deliver"
 	TypeImageResize   = "image:resize"
 )
-
-const (
-	PriorityDefault  = "default"
-	PriorityCritical = "critical"
-	PriorityLow      = "low"
-)
