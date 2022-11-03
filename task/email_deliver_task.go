aa3c82fadf036f4d2ac163d5ed7305cc08508f6e
pengdst <pengkuh.dst@gmail.com>
2022-11-04 01:00:59 +0700

    create tasks

---
+++ task/email_deliver_task.go
@@ -0,0 +1,37 @@
+package task
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hibiken/asynq"
+	"log"
+)
+
+const (
+	TypeEmailDelivery = "email:deliver"
+	TypeImageResize   = "image:resize"
+)
+
+type EmailDeliveryPayload struct {
+	UserID     int
+	TemplateID string
+}
+
+func NewEmailDeliveryTask(userID int, tmplID string) (*asynq.Task, error) {
+	payload, err := json.Marshal(EmailDeliveryPayload{UserID: userID, TemplateID: tmplID})
+	if err != nil {
+		return nil, err
+	}
+	return asynq.NewTask(TypeEmailDelivery, payload), nil
+}
+
+func HandleEmailDeliveryTask(ctx context.Context, t *asynq.Task) error {
+	var p EmailDeliveryPayload
+	if err := json.Unmarshal(t.Payload(), &p); err != nil {
+		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
+	}
+	log.Printf("Sending Email to User: user_id=%d, template_id=%s", p.UserID, p.TemplateID)
+	// Email delivery code ...
+	return nil
+}
---
+++ task/image_resize_task.go
@@ -0,0 +1,42 @@
+package task
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hibiken/asynq"
+	"log"
+	"time"
+)
+
+type ImageResizePayload struct {
+	SourceURL string
+}
+
+func NewImageResizeTask(src string) (*asynq.Task, error) {
+	payload, err := json.Marshal(ImageResizePayload{SourceURL: src})
+	if err != nil {
+		return nil, err
+	}
+	// task options can be passed to NewTask, which can be overridden at enqueue time.
+	return asynq.NewTask(TypeImageResize, payload, asynq.MaxRetry(5), asynq.Timeout(20*time.Minute)), nil
+}
+
+// ImageProcessor implements asynq.Handler interface.
+type ImageProcessor struct {
+	// ... fields for struct
+}
+
+func (processor *ImageProcessor) ProcessTask(ctx context.Context, t *asynq.Task) error {
+	var p ImageResizePayload
+	if err := json.Unmarshal(t.Payload(), &p); err != nil {
+		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
+	}
+	log.Printf("Resizing image: src=%s", p.SourceURL)
+	// Image resizing code ...
+	return nil
+}
+
+func NewImageProcessor() *ImageProcessor {
+	return &ImageProcessor{}
+}
