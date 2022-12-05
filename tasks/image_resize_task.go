76f59318d405ecc3b3659ea0801bbf751afa31f4
pengdst <pengkuh.dst@gmail.com>
2022-12-05 16:52:53 +0700

    resize image using image resize repository

--- tasks/image_resize_task.go
+++ tasks/image_resize_task.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"github.com/hibiken/asynq"
 	"log"
+	"os"
+	"task-queue-asynq/model/kraken"
+	"task-queue-asynq/repository"
 	taskType "task-queue-asynq/tasks/type"
 	"time"
 )
@@ -34,7 +37,32 @@ func (processor *ImageProcessor) ProcessTask(ctx context.Context, t *asynq.Task)
 		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
 	}
 	log.Printf("Resizing image: src=%s", p.SourceURL)
-	// Image resizing code ...
+
+	imageResizeRepository := repository.NewImageResizeRepository()
+
+	data, err := imageResizeRepository.Resize(kraken.Request{
+		Auth: kraken.Auth{
+			ApiKey:    os.Getenv("KRAKEN_API_KEY"),
+			ApiSecret: os.Getenv("KRAKEN_API_SECRET"),
+		},
+		Url:  p.SourceURL,
+		Wait: true,
+		Resize: kraken.Resize{
+			Width:    1080,
+			Height:   608,
+			Strategy: kraken.Auto,
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("resize image failed: %v: %w", err, asynq.SkipRetry)
+	}
+
+	if !data.Success {
+		return fmt.Errorf("Failed, error message: %v, %v ", err, data)
+	} else {
+		log.Println("Success, Optimized image URL: ", data.KrakedUrl)
+	}
 	return nil
 }
 
